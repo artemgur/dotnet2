9f85cda87d71f48060928b9c0ebc511e59482bc2
Gurianov Artem <artemgur01@gmail.com>
2020-09-07 20:10:49 +0300

    Calculator 1.0

---
+++ Calculator/Calculator/Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Calculator
+{
+	public static class Calculator
+	{
+		public static double Calculate(double a, double b, char @operator) => @operator switch
+		{
+			'+' => a + b,
+			'-' => a - b,
+			'*' => a * b,
+			'/' => a / b,
+			'^' => Math.Pow(a, b),
+			_=> throw new NotImplementedException("The operation doesn't exist")
+		};
+	}
+}
--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -1,9 +1,17 @@
-﻿namespace Calculator
+﻿using System;
+
+namespace Calculator
 {
-	internal class Program
+	public static class Program
 	{
 		public static void Main(string[] args)
 		{
+			var a = ReadDouble();
+			var @operator = char.Parse(Console.ReadLine());
+			var b = ReadDouble();
+			Console.WriteLine(Calculator.Calculate(a, b, @operator));
 		}
+
+		private static double ReadDouble() => double.Parse(Console.ReadLine());
 	}
 }
