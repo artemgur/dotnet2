d4964a8d218ae548116b825a170995315cbdde24
Gurianov Artem <artemgur01@gmail.com>
2020-09-14 16:12:28 +0300

    Revert "Merge pull request #2 from artemgur/revert-1-dr1"
    This reverts commit fb70569a702c82973a2dbbde1090d411644ed637, reversing
    changes made to ef46f5444ce5b459429a680930b05275b96cd8da.

---
+++ Calculator/Calculator/CalculatorTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Calculator
+{
+	public static class CalculatorTests
+	{
+		[TestCase(0, 0)]
+		[TestCase(2, 2)]
+		[TestCase(5, -7)]
+		public static void AdditionTest(double a, double b) => Assert.AreEqual(a + b, Calculator.Calculate(a, b, '+'));
+		
+		[TestCase(0, 0)]
+		[TestCase(2, 2)]
+		[TestCase(5, -7)]
+		public static void SubtractionTest(double a, double b) => Assert.AreEqual(a - b, Calculator.Calculate(a, b, '-'));
+		
+		[TestCase(0, 0)]
+		[TestCase(2, 2)]
+		[TestCase(5, -7)]
+		public static void MultiplicationTest(double a, double b) => Assert.AreEqual(a * b, Calculator.Calculate(a, b, '*'));
+		
+		[TestCase(0, 0)]
+		[TestCase(2, 2)]
+		[TestCase(5, -7)]
+		public static void DivisionTest(double a, double b) => Assert.AreEqual(a / b, Calculator.Calculate(a, b, '/'));
+		
+		[TestCase(0, 5)]
+		[TestCase(2, 2)]
+		[TestCase(5, -7)]
+		public static void PowerTest(double a, double b) => Assert.AreEqual(Math.Pow(a, b), Calculator.Calculate(a, b, '^'));
+	}
+}
