61091ea0dec70943ead13ec38b5c9a0b5f59c635
Gurianov Artem <artemgur01@gmail.com>
2020-09-14 16:18:35 +0300

    Made calculator

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Calculator
+{
+	public static class Program
+	{
+		public static void Main(string[] args)
+		{
+			var a = ReadDouble();
+			var @operator = char.Parse(Console.ReadLine());
+			var b = ReadDouble();
+			Console.WriteLine(Calculator.Calculate(a, b, @operator));
+		}
+
+		private static double ReadDouble() => double.Parse(Console.ReadLine());
+	}
+}
