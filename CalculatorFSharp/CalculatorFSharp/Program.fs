b6be936777c02cea88a87739662283cdb488e771
Artem Gurianov <artemgur01@gmail.com>
2020-09-27 22:34:05 +0300

    Removed exception when calculator gets invalid number

--- CalculatorFSharp/CalculatorFSharp/Program.fs
+++ CalculatorFSharp/CalculatorFSharp/Program.fs
@@ -20,6 +20,10 @@ let multiply x y = Some(x * y)
 
 let divide x y = if y = 0.0 then None else Some(x / y)
 
+let parse (str:string) =
+    let isNumber, num = Double.TryParse str
+    if isNumber then Some(num) else None
+
 let calculate op a b =
     maybe{
         let! x = match op with
@@ -31,13 +35,16 @@ let calculate op a b =
         return x
     }
 
-let write (t:float option) = if t=None then Console.WriteLine("None") else Console.WriteLine(t.Value)
+let write (t:float option) = if t=None then Console.WriteLine("Computational error. Probably you tried to divide by 0 or used invalid operator") else Console.WriteLine(t.Value)
+
+let calculateAndWrite op a b =
+    let t = calculate op a b
+    write t
 
 [<EntryPoint>]
 let main argv =
-    let a = Console.ReadLine() |> Double.Parse
+    let a = parse (Console.ReadLine())
     let op = Console.ReadLine()
-    let b = Console.ReadLine() |> Double.Parse
-    let t = calculate op a b
-    write t
+    let b = parse (Console.ReadLine())
+    if a = None || b = None then Console.WriteLine("One of parameters is not a number") else calculateAndWrite op a.Value b.Value
     0
