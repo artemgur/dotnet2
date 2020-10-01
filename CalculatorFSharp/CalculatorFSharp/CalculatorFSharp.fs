4acfb59cee48f6829f7062aa594146451cc91cbb
artemgur <artemgur01@gmail.com>
2020-10-01 21:01:08 +0300

    Added tests to F# Calculator, some edits in F# Calculator code, edited GitHub actions to display test coverage in F# Calculator

--- CalculatorFSharp/CalculatorFSharp/CalculatorFSharp.fs
+++ CalculatorFSharp/CalculatorFSharp/CalculatorFSharp.fs
@@ -1,4 +1,6 @@
-﻿open System
+﻿module CalculatorFSharp
+
+open System
 
 type Maybe() =
 
@@ -7,18 +9,18 @@ type Maybe() =
         | None -> None
         | Some a -> f a
 
-    member this.Return x = 
+    member this.Return(x) = 
         Some x
         
 let argsToTuple a b = if a=None || b=None then None else Some(a.Value,b.Value)
         
 let maybe = Maybe()
 
-let add (x:float*float) = Some(fst x + snd x)
+let add (x:float*float) = fst x + snd x
 
-let subtract (x:float*float) = Some(fst x - snd x)
+let subtract (x:float*float) = fst x - snd x
 
-let multiply (x:float*float) = Some(fst x * snd x)
+let multiply (x:float*float) = fst x * snd x
 
 let divide (x:float*float) = if snd x = 0.0 then None else Some(fst x / snd x)
 
@@ -26,28 +28,25 @@ let parse (str:string) =
     let isNumber, num = Double.TryParse str
     if isNumber then Some(num) else None
 
-let calculate op (a:float option) (b:float option) =
+let calculate op a b =
     maybe{
         let! tuple = argsToTuple a b
         let! x = match op with
-            | "+" -> add tuple
-            | "-" -> subtract tuple
-            | "*" -> multiply tuple
-            | "/" -> divide tuple
-            | _ -> None
+                    | "+" -> Some(add tuple)
+                    | "-" -> Some(subtract tuple)
+                    | "*" -> Some(multiply tuple)
+                    | "/" -> divide tuple
+                    | _ -> None
         return x
     }
 
 let write (t:float option) = if t=None then Console.WriteLine("Computational error. Probably you tried to divide by 0 or inputted invalid number or operator") else Console.WriteLine(t.Value)
 
-let calculateAndWrite op a b =
-    let t = calculate op a b
-    write t
-
 [<EntryPoint>]
-let main argv =
+let main _ =
     let a = parse (Console.ReadLine())
     let op = Console.ReadLine()
     let b = parse (Console.ReadLine())
-    calculateAndWrite op a b
+    let t = calculate op a b
+    write t
     0
