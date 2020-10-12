74402f2233c8290cd99f00fb766b6eb863b60a2d
artemgur <artemgur01@gmail.com>
2020-10-12 16:17:45 +0300

    Update CalculatorMiddleware.cs

--- CalculatorASPNET/CalculatorASPNET/CalculatorMiddleware.cs
+++ CalculatorASPNET/CalculatorASPNET/CalculatorMiddleware.cs
@@ -21,7 +21,7 @@ namespace Calculator
 			{
 				var a = Calculator.Calculate(context.Request.Query[parameterName]);
 				context.Response.Headers.Add("calculator_result", a);
-				if (!double.TryParse(a, out var x))
+				if (a == Calculator.ErrorInvalidNumberString || a == Calculator.ErrorNoOperatorString || a == Calculator.ErrorDivideByZeroString)
 					context.Response.StatusCode = 400;
 				next.Invoke(context);
 			}
