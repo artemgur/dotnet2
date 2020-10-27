ce7b386098d7b6cfeda29abf7e874bf96affde27
artemgur <artemgur01@gmail.com>
2020-10-27 23:30:07 +0300

    Done expression parser. Parallel part of task isn't done yet

---
+++ CalculatorASPNET/CalculatorExpression/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace CalculatorExpression
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var expression = ExpressionTreeBuilder.Build("1.5/5");
+			var lambda = Expression.Lambda<Func<double>>(expression);
+			var compiled = lambda.Compile();
+			Console.WriteLine(compiled());
+		}
+	}
+}
